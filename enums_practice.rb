eee19c344b41e0c3361e8d3554f6d9c896e7de53
Stephan Constantial <Sconstantial@yahoo.com>
2018-09-08 22:45:05 +0100

    lesson completed

--- enums_practice.rb
+++ enums_practice.rb
@@ -37,6 +37,33 @@ pets = [
 }
 ]
 
-for pet in pets
-p pet [:name]
-end
+# for pet in pets
+# p pet [:name]
+# end
+ #
+ # pets.each do |pet|
+ #   p pet [:name]
+ # end
+
+#
+# for pet in pets
+#   if pet[:name] ==  "Merlin"
+# end
+# end
+# p pet
+#
+# p pets.find {|pet| pet[:name] == "Merlin"}
+
+p pets.find{|pet| pet[:breed] == "Husky"}
+
+p pets.map {|pet| pet[:name] }
+
+p pets.find {|pet| pet[:breed] == "Dalmation"}
+
+p pets.any? {|pet| pet[:breed] == "Dalmation"}
+
+p pets.max {|pet| pet[:price]}
+
+p pets.reduce(0) {|sum, pet| sum + pet[:price]}
+
+p pets.each {|pet| pet[:price] /=2}
